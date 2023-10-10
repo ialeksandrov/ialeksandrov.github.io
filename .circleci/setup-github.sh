924491fed0dbfb9eb1ead56996d1ea9a240d46cf
ialeksandrov <ialeksandrov1990@abv.bg>
2023-10-10 17:12:42 +0300

    Modify setup-github.sh script

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-USER_EMAIL='ialeksandrov1990@abv.bg'
-USER_NAME='ialeksandrov'
-
-
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
-
 git checkout master
 git pull origin master
 
